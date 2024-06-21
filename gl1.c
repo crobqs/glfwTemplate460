cd484bf8d6203fd9ac1117d9d6470efc7b83b68f
crobqs <srustimahanta10@gmail.com>
2024-06-21 10:12:21 +0530

    texture

--- gl1.c
+++ gl1.c
@@ -158,41 +158,70 @@ static void run(void) {
     glfwSetKeyCallback(window, key_callback);
     glfwSetFramebufferSizeCallback(window, framebuffersize_callback);
 
-    GLuint program = compile("default.vert", "default.frag");
+    GLuint program = compile("shaders/tex.vert", "shaders/tex.frag");
 
     GLfloat vertices[] = {
-        -1.0f, -1.0f,    1.0f, 0.3f, 0.0f, /* bootom-left  */
-        -1.0f,  1.0f,    0.5f, 0.7f, 0.3f, /* top-left     */
-         1.0f,  1.0f,    0.7f, 0.9f, 0.1f, /* top-right    */
-         1.0f, -1.0f,    0.6f, 0.1f, 1.0f  /* bottom-right */
+        -1.0f, -1.0f,    1.0f, 0.3f, 0.0f,    0.0f, 0.0f, /* bootom-left  */
+        -1.0f,  1.0f,    0.5f, 0.7f, 0.3f,    0.0f, 1.0f, /* top-left     */
+         1.0f,  1.0f,    0.7f, 0.9f, 0.1f,    1.0f, 1.0f, /* top-right    */
+         1.0f, -1.0f,    0.6f, 0.1f, 1.0f,    1.0f, 0.0f  /* bottom-right */
     };
+    
     GLuint indices[] = {
         0, 1, 3,
         1, 2, 3
     };
 
     GLuint VAO, VBO, EBO;
-    glGenVertexArrays(1, &VAO);
-    glGenBuffers(1, &VBO);
-    glGenBuffers(1, &EBO);
-    glBindVertexArray(VAO);
-    glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-    glBufferData(GL_ARRAY_BUFFER, sizeof vertices, vertices, GL_STATIC_DRAW);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof indices, indices, GL_STATIC_DRAW);
-    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(GLfloat), (void*)0);
-    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(GLfloat), (void*)(2 * sizeof(GLfloat)));
-    glEnableVertexAttribArray(0);
-    glEnableVertexAttribArray(1);
-    glBindVertexArray(0);
-    glBindBuffer(GL_ARRAY_BUFFER, 0);
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+    
+    {
+        glGenVertexArrays(1, &VAO);
+        glGenBuffers(1, &VBO);
+        glGenBuffers(1, &EBO);
+        glBindVertexArray(VAO);
+        glBindBuffer(GL_ARRAY_BUFFER, VBO);
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+        glBufferData(GL_ARRAY_BUFFER, sizeof vertices, vertices, GL_STATIC_DRAW);
+        glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof indices, indices, GL_STATIC_DRAW);
+        glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 7 * sizeof(GLfloat), (void*)0);
+        glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 7 * sizeof(GLfloat), (void*)(2 * sizeof(GLfloat)));
+        glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 7 * sizeof(GLfloat), (void*)(5 * sizeof(GLfloat)));
+        glEnableVertexAttribArray(0);
+        glEnableVertexAttribArray(1);
+        glEnableVertexAttribArray(2);
+        glBindVertexArray(0);
+        glBindBuffer(GL_ARRAY_BUFFER, 0);
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+    }
+
+    int width, height, channels;
+    const char * tex1_FileName = "sunsetClouds.jpg";
+    GLuint tex1;
+    stbi_set_flip_vertically_on_load(true);
+
+    {
+        glGenTextures(1, &tex1);
+        glBindTexture(GL_TEXTURE_2D, tex1);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+        unsigned char * data = stbi_load(tex1_FileName, &width, &height, &channels, 4);
+        if (data) {
+            glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
+            glGenerateMipmap(GL_TEXTURE_2D);
+            stbi_image_free(data);
+        } else {
+            fprintf(stderr, "(%s, %d) Failed to load texture \"%s\"\n", __func__, __LINE__, tex1_FileName);
+        }
+    }
 
     while (!glfwWindowShouldClose(window)) {
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
         glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
         glUseProgram(program);
         glBindVertexArray(VAO);
+        glBindTexture(GL_TEXTURE_2D, tex1);
         glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, NULL);
         glfwSwapBuffers(window);
         glfwPollEvents();
@@ -201,6 +230,7 @@ static void run(void) {
     glDeleteProgram(program);
     glDeleteVertexArrays(1, &VAO);
     glDeleteBuffers(1, &VBO);
+    glDeleteBuffers(1, &EBO);
     glfwTerminate();
 }
 
---
+++ shaders/tex.frag
@@ -0,0 +1,12 @@
+#version 460 core
+
+in vec3 Color;
+in vec2 TexCoord;
+
+out vec4 FragColor;
+
+uniform sampler2D Tex;
+
+void main() {
+    FragColor = texture(Tex, TexCoord);
+}
